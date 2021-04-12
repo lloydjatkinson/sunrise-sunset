446222fdf3658702a04c7c122bf7576438d86834
Lloyd Atkinson <lloydjatkinson@gmail.com>
2021-04-12 17:45:21 +0100

    Initial Vue CLI setup

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/base',
+        'plugin:vue/vue3-essential',
+        'plugin:vue/vue3-recommended',
+        'plugin:vue/vue3-strongly-recommended',
+        '@vue/airbnb',
+    ],
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+        // Airbnb style guide with some minor tweaks.
+        indent: ['error', 4],
+        'linebreak-style': 'off',
+        'eol-last': ['error', 'never'],
+        'max-len': ['error', { code: 450 }],
+        'space-before-function-paren': ['error', 'always'],
+        'no-unused-vars': 'off',
+
+        // Allow for..in and for..of that Airbnb discourages.
+        'no-iterator': 'off',
+        'no-restricted-syntax': ['error', 'ForInStatement'],
+
+        // These two are because of the requires from some generated files from Vue CLI Tailwind plugin.
+        'global-require': 'off',
+        'import/no-extraneous-dependencies': 'off',
+
+        // Vue specific
+        'vue/html-indent': ['error', 4, {
+            attribute: 1,
+            closeBracket: 0,
+            alignAttributesVertically: true,
+            ignores: [],
+        }],
+
+        'vue/html-closing-bracket-newline': ['error', {
+            singleline: 'never',
+            multiline: 'never',
+        }],
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)',
+                '**/*.spec.{j,t}s?(x)',
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
+};
---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+    <img
+        alt="Vue logo"
+        src="./assets/logo.png">
+    <h1>Welcome to Your Vue.js App</h1>
+</template>
